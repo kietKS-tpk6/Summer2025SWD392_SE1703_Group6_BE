39cb57e1d977004bb5222edc0ee803d0681c908e
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-06-01 23:54:06 +0700

    Create updateSubjectCommand

---
+++ Application/Usecases/Command/UpdateSubjectCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace Application.Usecases.Command
+{
+    public class UpdateSubjectCommand : IRequest<string>
+    {
+        public string SubjectID { get; set; }
+        public string SubjectName { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+        public float MinAverageScoreToPass { get; set; }
+    }
+}
