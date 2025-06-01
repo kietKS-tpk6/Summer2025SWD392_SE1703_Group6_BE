90dfabfd7815dbe3171a3cc8cae6dcd824e476f6
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-06-02 00:01:50 +0700

    create ISubjectService

---
+++ Infrastructure/IRepositories/ISubjectRepository.cs
@@ -0,0 +1,14 @@
+﻿using Application.DTOs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application.IServices
+{
+    public interface ISubjectService
+    {
+        Task<List<SubjectDTO>> GetAllSubjectsAsync(bool? isActive = null);
+        Task<SubjectDTO> GetSubjectByIdAsync(string subjectId);
+        Task<bool> SubjectExistsAsync(string subjectId);
+        Task<int> GetTotalSubjectsCountAsync();
+    }
+}
