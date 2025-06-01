acb4ebb634049e8c8950bb1b236efd130e787d51
kietKS-tpk6 <a.kiet096@gmail.com>
2025-06-01 21:55:00 +0700

    feat create syllabus

---
+++ Application/IServices/ISyllabusesService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Usecases.Command;
+
+namespace Application.IServices
+{
+    public interface ISyllabusesService
+    {
+        public Task<string> createSyllabuses(CreateSyllabusesCommand createSyllabusesCommand); 
+    }
+}
---
+++ Infrastructure/Repositories/SyllabusesRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.DTOs;
+using Application.Usecases.Command;
+using CloudinaryDotNet;
+using Domain.Entities;
+using Infrastructure.Data;
+using Infrastructure.IRepositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class SyllabusesRepository : ISyllabusesRepository
+    {
+        private readonly HangulLearningSystemDbContext _dbContext;
+
+        public SyllabusesRepository(HangulLearningSystemDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> CreateSyllabusesAsync(Syllabus syllabus)
+        {
+            await _dbContext.Syllabus.AddAsync(syllabus);
+            await _dbContext.SaveChangesAsync();
+            return true;
+          }
+
+        public async Task<int> GetNumbeOfSyllabusAsync()
+        {
+
+            return await _dbContext.Syllabus.CountAsync();
+        }
+
+
+
+    }
+}
