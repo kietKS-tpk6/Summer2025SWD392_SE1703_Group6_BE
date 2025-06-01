46de0dc1443270fcbc66c2f90c02b07d5a1042df
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-06-01 23:50:30 +0700

    create subjectDTO

---
+++ Application/DTOs/SubjectDTO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Application.DTOs
+{
+    public class SubjectDTO
+    {
+        public string SubjectID { get; set; }
+        public string SubjectName { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreateAt { get; set; }
+        public float MinAverageScoreToPass { get; set; }
+    }
+
+    public class CreateSubjectDTO
+    {
+        public string SubjectID { get; set; }
+        public string SubjectName { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; } = true;
+        public float MinAverageScoreToPass { get; set; } = 5.0f;
+    }
+
+    public class UpdateSubjectDTO
+    {
+        public string SubjectID { get; set; }
+        public string SubjectName { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+        public float MinAverageScoreToPass { get; set; }
+    }
+}
