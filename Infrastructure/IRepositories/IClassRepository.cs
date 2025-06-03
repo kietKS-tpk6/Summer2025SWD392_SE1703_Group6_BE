a0fe9ab089dcb051bee6337747839b042b8d4ae9
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-06-03 14:22:35 +0700

    fix IClassRepository

--- Infrastructure/IRepositories/IClassRepository.cs
+++ Infrastructure/IRepositories/IClassRepository.cs
@@ -15,5 +15,14 @@ namespace Infrastructure.IRepositories
         Task<bool> UpdateAsync(Class classUpdate);
         Task<bool> DeleteAsync(string id);
         Task<int> CountAsync();
+
+        Task<string> CreateClassAsync(Class classEntity);
+        Task<Class> GetClassByIdAsync(string classId);
+        Task<List<Class>> GetAllClassesAsync(bool includeInactive = false);
+        Task<List<Class>> GetClassesBySubjectIdAsync(string subjectId);
+        Task<string> UpdateClassAsync(Class classEntity);
+        Task<bool> ClassExistsAsync(string classId);
+        Task<int> GetTotalClassesCountAsync();
+        Task<string> GenerateNextClassIdAsync();
     }
 }
