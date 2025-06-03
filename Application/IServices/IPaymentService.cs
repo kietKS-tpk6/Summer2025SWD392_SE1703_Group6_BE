a5eee0f871a9b4f298a9f47dd70991eb28d4d8bf
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-06-03 13:52:56 +0700

    create IPaymentService

---
+++ Application/IServices/IPaymentService.cs
@@ -0,0 +1,19 @@
+﻿using Application.DTOs;
+using Application.Usecases.Command;
+using Domain.Entities;
+using System.Threading.Tasks;
+
+namespace Application.IServices
+{
+    public interface IPaymentService
+    {
+        Task<PaymentResponseDTO> CreatePaymentAsync(CreatePaymentCommand command);
+        Task<PaymentStatusDTO> CheckPaymentStatusAsync(string paymentId);
+        Task<string> GenerateNextPaymentIdAsync();
+        Task<WebhookResponseDTO> ProcessWebhookAsync(TransactionDTO transaction);
+        Task<bool> UpdatePaymentStatusAsync(string paymentId, decimal amountReceived);
+        Task<Payment> GetPaymentAsync(string paymentId);
+        string GetQrCodeUrl(string paymentId, decimal amount);
+        string GetWebhookUrl();
+    }
+}
