8944ee6aa8aab6f80932997bf54b4c02a88697cc
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-06-03 14:10:40 +0700

    create ProcessWebhookCommandHandler

---
+++ Application/Usecases/CommandHandler/ProcessWebhookCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Application.DTOs;
+using Application.IServices;
+using Application.Usecases.Command;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Usecases.CommandHandler
+{
+    public class ProcessWebhookCommandHandler : IRequestHandler<ProcessWebhookCommand, WebhookResponseDTO>
+    {
+        private readonly IPaymentService _paymentService;
+
+        public ProcessWebhookCommandHandler(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        public async Task<WebhookResponseDTO> Handle(ProcessWebhookCommand request, CancellationToken cancellationToken)
+        {
+            return await _paymentService.ProcessWebhookAsync(request.Transaction);
+        }
+    }
+}
