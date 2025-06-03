2e958c3d438d3d89fed436b2e00a678552df5ec1
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-06-03 13:55:22 +0700

    create IPaymentRepository

---
+++ Infrastructure/IRepositories/IPaymentRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Infrastructure.IRepositories
+{
+    public interface IPaymentRepository
+    {
+        Task<string> CreatePaymentAsync(Payment payment);
+        Task<Payment> GetPaymentByIdAsync(string paymentId);
+        Task<List<Payment>> GetPaymentsByAccountIdAsync(string accountId);
+        Task<string> UpdatePaymentAsync(Payment payment);
+        Task<bool> PaymentExistsAsync(string paymentId);
+        Task<int> GetTotalPaymentsCountAsync();
+    }
+}
