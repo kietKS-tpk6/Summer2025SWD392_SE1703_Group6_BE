dfc0ee1d0c04f4278ae4567f562d00d8f2db1d90
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-06-03 14:21:25 +0700

    fix IClassService

--- Application/IServices/IClassService.cs
+++ Application/IServices/IClassService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Application.DTOs;
 using Application.Usecases.Command;
 
 namespace Application.IServices
@@ -10,8 +11,11 @@ namespace Application.IServices
     public interface IClassService
     {
         Task<bool> CreateClassAsync(ClassCreateCommand  request);
-        
 
+        Task<List<ClassDetailForPaymentDTO>> GetAllClassesAsync(bool includeInactive = false);
+        Task<ClassDetailForPaymentDTO> GetClassDetailAsync(string classId);
+        Task<bool> IsClassAvailableForEnrollmentAsync(string classId);
+        Task<int> GetAvailableSlotsAsync(string classId);
 
     }
 }
