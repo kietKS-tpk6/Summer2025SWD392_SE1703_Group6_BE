42e58bab685bf98c1b92cb803ba81662c63c6db1
Khoa Vo <khoavnase183164@fpt.edu.vn>
2025-06-06 21:25:16 +0700

    feat(validator): add LessonCreateCommandValidator and LessonUpdateCommandValidator

---
+++ Application/Validators/LessonCreateCommandValidator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using FluentValidation;
+using Application.Common.Constants;
+using Application.Usecases.Command;
+
+namespace Application.Validators
+{
+    public class LessonCreateCommandValidator : AbstractValidator<LessonCreateCommand>
+    {
+        public LessonCreateCommandValidator()
+        {
+            RuleFor(x => x.ClassID)
+                .NotEmpty()
+                .WithErrorCode(nameof(ErrorCodes.ClassIDIsEmpty))
+                .WithMessage(ValidationMessages.ClassIDIsEmpty);
+
+            RuleFor(x => x.SyllabusScheduleID)
+                .NotEmpty()
+                .WithErrorCode(ErrorCodes.SyllabusScheduleIDIsEmpty)
+                .WithMessage(ValidationMessages.SyllabusScheduleIDIsEmpty);
+
+            RuleFor(x => x.LecturerID)
+                .NotEmpty()
+                .WithErrorCode(nameof(ErrorCodes.LecturerIDIsEmpty))
+                .WithMessage(ValidationMessages.LecturerIDIsEmpty);
+
+            RuleFor(x => x.StartTime)
+                .GreaterThan(DateTime.Now)
+                .WithErrorCode(ErrorCodes.LessonStartTimeInvalid)
+                .WithMessage(ValidationMessages.LessonStartTimeInvalid);
+        }
+    }
+}
---
+++ Application/Validators/LessonUpdateCommandValidator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using FluentValidation;
+using Application.Common.Constants;
+using Application.Usecases.Command;
+
+namespace Application.Validators
+{
+    public class LessonUpdateCommandValidator : AbstractValidator<LessonUpdateCommand>
+    {
+        public LessonUpdateCommandValidator()
+        {
+            RuleFor(x => x.ClassLessonID)
+                .NotEmpty()
+                .WithErrorCode(nameof(ErrorCodes.ClassLessonIDIsEmpty))
+                .WithMessage(ValidationMessages.ClassLessonIDIsEmpty);
+
+            RuleFor(x => x.ClassID)
+                .NotEmpty()
+                .WithErrorCode(nameof(ErrorCodes.ClassIDIsEmpty))
+                .WithMessage(ValidationMessages.ClassIDIsEmpty);
+
+            RuleFor(x => x.SyllabusScheduleID)
+                .NotEmpty()
+                .WithErrorCode(ErrorCodes.SyllabusScheduleIDIsEmpty)
+                .WithMessage(ValidationMessages.SyllabusScheduleIDIsEmpty);
+
+            RuleFor(x => x.LecturerID)
+                .NotEmpty()
+                .WithErrorCode(nameof(ErrorCodes.LecturerIDIsEmpty))
+                .WithMessage(ValidationMessages.LecturerIDIsEmpty);
+
+            RuleFor(x => x.StartTime)
+                .GreaterThan(DateTime.Now)
+                .WithErrorCode(ErrorCodes.LessonStartTimeInvalid) 
+                .WithMessage(ValidationMessages.LessonStartTimeInvalid);
+        }
+    }
+}
