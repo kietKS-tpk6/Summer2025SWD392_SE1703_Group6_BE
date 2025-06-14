3b98e530cfabf6e1ef4d241c1b17ee0c83a1450b
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-06-14 10:45:48 +0700

    create CheckSubjectStatusCommand

---
+++ Application/Usecases/Command/CheckSubjectStatusCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace Application.Usecases.Command
+{
+    public class CheckSubjectStatusCommand : IRequest<SubjectStatusCheckResult>
+    {
+        public string SubjectID { get; set; }
+    }
+
+    public class SubjectStatusCheckResult
+    {
+        public bool CanActivate { get; set; }
+        public bool HasSchedule { get; set; }
+        public bool HasAssessmentCriteria { get; set; }
+        public List<string> MissingFields { get; set; } = new List<string>();
+        public string Message { get; set; }
+    }
+}
