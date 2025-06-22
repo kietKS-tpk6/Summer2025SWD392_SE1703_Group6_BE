c1841c85d32959cb00f3672db6e8189b15b63698
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-06-22 15:31:20 +0700

    create CreateTestRequestDTO

---
+++ Application/DTOs/CreateTestRequestDTO.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Enums;
+
+namespace Application.DTOs
+{
+    public class CreateTestRequestDTO
+    {
+        public string SubjectID { get; set; }
+        public TestType TestType { get; set; }
+        public TestCategory Category { get; set; }
+    }
+}
