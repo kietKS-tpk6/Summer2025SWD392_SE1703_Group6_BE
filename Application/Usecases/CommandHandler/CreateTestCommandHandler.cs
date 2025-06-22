0a43c3600545490db7cb60be48579f80ba1793dc
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-06-22 16:01:05 +0700

    create CreateTestCommandHandler

---
+++ Application/Usecases/CommandHandler/CreateTestCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Application.IServices;
+using Application.Usecases.Command;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Usecases.CommandHandler
+{
+    public class CreateTestCommandHandler : IRequestHandler<CreateTestCommand, string>
+    {
+        private readonly ITestService _testService;
+
+        public CreateTestCommandHandler(ITestService testService)
+        {
+            _testService = testService;
+        }
+
+        public async Task<string> Handle(CreateTestCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _testService.CreateTestAsync(request);
+
+            if (result.Success)
+                return result.Data;
+
+            throw new ArgumentException(result.Message);
+        }
+    }
+}
