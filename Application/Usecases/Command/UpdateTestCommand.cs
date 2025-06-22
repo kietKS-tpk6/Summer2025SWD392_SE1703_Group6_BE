837ed4ff10e1e6cecc50c32b62f1f68536b0d912
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-06-22 15:38:50 +0700

    create UpdateTestCommand

---
+++ Application/Usecases/Command/UpdateTestCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Application.Usecases.Command
+{
+    public class UpdateTestCommand : IRequest<string>
+    {
+        public string TestID { get; set; }
+        public string TestName { get; set; }
+        public string RequestingAccountID { get; set; }
+    }
+}
