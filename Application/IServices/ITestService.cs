e936230667a13c70a9adf8fff6d019a9b4521065
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-06-22 15:57:47 +0700

    create ITestService

---
+++ Application/IServices/ITestService.cs
@@ -0,0 +1,20 @@
+﻿using Application.Usecases.Command;
+using Application.Common.Constants;
+using Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application.IServices
+{
+    public interface ITestService
+    {
+        Task<OperationResult<string>> CreateTestAsync(CreateTestCommand command);
+        Task<OperationResult<string>> UpdateTestAsync(UpdateTestCommand command);
+        Task<OperationResult<string>> UpdateTestStatusAsync(UpdateTestStatusCommand command);
+        Task<OperationResult<string>> DeleteTestAsync(DeleteTestCommand command);
+        Task<OperationResult<Test>> GetTestByIdAsync(string testId);
+        Task<OperationResult<List<Test>>> GetTestsByAccountIdAsync(string accountId);
+        Task<string> GenerateNextTestIdAsync();
+        Task ProcessAutoApprovalAsync();
+    }
+}
