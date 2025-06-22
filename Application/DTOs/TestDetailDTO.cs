b777f966884dc8784ea6cbc1045199e189ea3d83
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-06-22 15:30:14 +0700

    create TestDetailDTO

---
+++ Application/DTOs/TestDetailDTO.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Enums;
+using System;
+using System.Collections.Generic;
+
+namespace Application.DTOs
+{
+    public class TestDetailDTO
+    {
+        public string TestID { get; set; }
+        public string CreateBy { get; set; }
+        public string CreatedByName { get; set; }
+        public string SubjectID { get; set; }
+        public string SubjectName { get; set; }
+        public DateTime CreateAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+        public TestStatus Status { get; set; }
+        public TestCategory Category { get; set; }
+        public TestType TestType { get; set; }
+        public List<TestSectionResponseDTO> TestSections { get; set; } = new List<TestSectionResponseDTO>();
+    }
+}
