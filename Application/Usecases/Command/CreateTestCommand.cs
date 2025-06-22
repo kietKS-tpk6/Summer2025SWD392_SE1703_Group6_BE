8e772926e0e83c360135cf2267cd11cb9e4e4c36
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-06-22 15:38:23 +0700

    create CreateTestCommand

---
+++ Application/Usecases/Command/CreateTestCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Domain.Enums;
+
+namespace Application.Usecases.Command
+{
+    public class CreateTestCommand : IRequest<string>
+    {
+        public string AccountID { get; set; }
+        public string SubjectID { get; set; }
+        public TestType TestType { get; set; }
+        public TestCategory Category { get; set; }
+    }
+}
