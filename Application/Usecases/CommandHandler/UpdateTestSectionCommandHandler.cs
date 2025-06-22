5efb6987be23d4a38a8304111aef2b50920e8705
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-06-22 19:07:19 +0700

    create CRUD commandhandler for testSection

---
+++ Application/Usecases/CommandHandler/UpdateTestSectionCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Application.IServices;
+using Application.Usecases.Command;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Usecases.CommandHandler
+{
+    public class UpdateTestSectionCommandHandler : IRequestHandler<UpdateTestSectionCommand, string>
+    {
+        private readonly ITestSectionService _testSectionService;
+
+        public UpdateTestSectionCommandHandler(ITestSectionService testSectionService)
+        {
+            _testSectionService = testSectionService;
+        }
+
+        public async Task<string> Handle(UpdateTestSectionCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _testSectionService.UpdateTestSectionAsync(request);
+
+            if (result.Success)
+                return result.Message;
+
+            throw new ArgumentException(result.Message);
+        }
+    }
+}
