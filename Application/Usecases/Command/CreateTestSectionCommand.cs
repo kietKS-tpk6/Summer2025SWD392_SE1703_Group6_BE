7fc93024bcbc3f2951efef4ec4e352260a50630d
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-06-22 17:45:45 +0700

    create command for testSection

---
+++ Application/Usecases/Command/CreateTestSectionCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Domain.Enums;
+
+namespace Application.Usecases.Command
+{
+    public class CreateTestSectionCommand : IRequest<string>
+    {
+        public string TestID { get; set; }
+        public string Context { get; set; }
+        public string? ImageURL { get; set; }
+        public string? AudioURL { get; set; }
+        public TestFormatType TestSectionType { get; set; }
+        public decimal? Score { get; set; }
+        public string RequestingAccountID { get; set; }
+    }
+}
---
+++ Application/Usecases/Command/DeleteTestSectionCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Application.Usecases.Command
+{
+    public class DeleteTestSectionCommand : IRequest<string>
+    {
+        public string TestSectionID { get; set; }
+        public string RequestingAccountID { get; set; }
+    }
+}
