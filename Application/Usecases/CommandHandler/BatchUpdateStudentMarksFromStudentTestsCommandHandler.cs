0925b945c015353956c4c635f38ddb650c6efc70
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-06-30 21:08:49 +0700

    create BatchUpdateStudentMarksFromStudentTestsCommandHandler

---
+++ Application/Usecases/CommandHandler/BatchUpdateStudentMarksFromStudentTestsCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Application.Common.Constants;
+using Application.DTOs;
+using Application.IServices;
+using Application.Usecases.Commands;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Usecases.CommandHandlers
+{
+    public class BatchUpdateStudentMarksFromStudentTestsCommandHandler : IRequestHandler<BatchUpdateStudentMarksFromStudentTestsCommand, OperationResult<BatchUpdateResultDTO>>
+    {
+        private readonly IStudentMarksService _studentMarksService;
+
+        public BatchUpdateStudentMarksFromStudentTestsCommandHandler(IStudentMarksService studentMarksService)
+        {
+            _studentMarksService = studentMarksService;
+        }
+
+        public async Task<OperationResult<BatchUpdateResultDTO>> Handle(BatchUpdateStudentMarksFromStudentTestsCommand request, CancellationToken cancellationToken)
+        {
+            return await _studentMarksService.BatchUpdateStudentMarksFromStudentTestsAsync(
+                request.StudentTests,
+                request.AssessmentCriteriaId,
+                request.ClassId);
+        }
+    }
+}
