d80c9c45d616e9560cc36eefd4842dd2f9ff9f92
kietKS-tpk6 <a.kiet096@gmail.com>
2025-06-30 19:43:40 +0700

    refactor dto

---
+++ Application/DTOs/MCQOptionForCreateDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class MCQOptionForCreateDTO
+    {
+        public string? Context { get; set; }
+        public string? ImageURL { get; set; }
+        public string? AudioURL { get; set; }
+        public bool IsCorrect { get; set; }
+    }
+}
