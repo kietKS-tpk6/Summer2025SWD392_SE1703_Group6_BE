bf79d9f40badf0482af0656913ef3b921746c62a
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-06-30 21:07:54 +0700

    create UpdateStudentMarksFromStudentTestCommandHandler

---
+++ Application/Usecases/CommandHandler/UpdateStudentMarksFromStudentTestCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Application.Common.Constants;
+using Application.IServices;
+using Application.Usecases.Commands;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Usecases.CommandHandlers
+{
+    public class UpdateStudentMarksFromStudentTestCommandHandler : IRequestHandler<UpdateStudentMarksFromStudentTestCommand, OperationResult<string>>
+    {
+        private readonly IStudentMarksService _studentMarksService;
+
+        public UpdateStudentMarksFromStudentTestCommandHandler(IStudentMarksService studentMarksService)
+        {
+            _studentMarksService = studentMarksService;
+        }
+
+        public async Task<OperationResult<string>> Handle(UpdateStudentMarksFromStudentTestCommand request, CancellationToken cancellationToken)
+        {
+            return await _studentMarksService.UpdateStudentMarksFromStudentTestAsync(
+                request.StudentTestId,
+                request.AssessmentCriteriaId,
+                request.ClassId);
+        }
+    }
+}
