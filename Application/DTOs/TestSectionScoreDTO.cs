6b8b25c5fcd02716eb9967aa7cef644c09603549
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-06-30 21:03:44 +0700

    create TestSectionScoreDTO

---
+++ Application/DTOs/TestSectionScoreDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.DTOs
+{
+    public class TestSectionScoreDTO
+    {
+        public string TestSectionId { get; set; }
+        public string Context { get; set; }
+        public decimal Score { get; set; }
+    }
+}
