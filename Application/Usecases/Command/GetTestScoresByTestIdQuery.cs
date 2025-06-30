019109ac094ab40fe02f6f1e53926bc61280a7d7
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-06-30 21:05:35 +0700

    create GetTestScoresByTestIdQuery

---
+++ Application/Usecases/Command/GetTestScoresByTestIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using Application.Common.Constants;
+using Application.DTOs;
+using MediatR;
+
+namespace Application.Usecases.Queries
+{
+    public class GetTestScoresByTestIdQuery : IRequest<OperationResult<GetTestScoresDTO>>
+    {
+        public string TestId { get; set; }
+    }
+}
