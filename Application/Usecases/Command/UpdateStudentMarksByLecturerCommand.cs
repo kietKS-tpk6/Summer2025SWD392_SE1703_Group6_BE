be9b5e2b6d15f3b7625993fd6850da14893fa7ed
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-06-30 21:06:16 +0700

    create UpdateStudentMarksByLecturerCommand

---
+++ Application/Usecases/Command/UpdateStudentMarksByLecturerCommand.cs
@@ -0,0 +1,13 @@
+﻿using Application.Common.Constants;
+using MediatR;
+
+namespace Application.Usecases.Commands
+{
+    public class UpdateStudentMarksByLecturerCommand : IRequest<OperationResult<bool>>
+    {
+        public string StudentMarkId { get; set; }
+        public decimal Mark { get; set; }
+        public string Comment { get; set; }
+        public string LecturerId { get; set; }
+    }
+}
