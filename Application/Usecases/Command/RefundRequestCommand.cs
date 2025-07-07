996cc17e19f88cc020c308543275befab3ffead7
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-07-07 13:19:06 +0700

    create RefundRequestCommand

---
+++ Application/Usecases/Command/RefundRequestCommand.cs
@@ -0,0 +1,12 @@
+﻿using Application.DTOs;
+using MediatR;
+
+namespace Application.Usecases.Command
+{
+    public class RefundRequestCommand : IRequest<RefundResponseDTO>
+    {
+        public string PaymentID { get; set; }
+        public string StudentID { get; set; }
+        public string Reason { get; set; }
+    }
+}
