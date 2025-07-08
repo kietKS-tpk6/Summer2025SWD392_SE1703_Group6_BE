c13167850c43f0b70054b3c4a927dcc80c0619fe
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-07-08 19:41:11 +0700

    create UpdateTaskStatusCommand

---
+++ Application/Usecases/Command/UpdateTaskStatusCommand.cs
@@ -0,0 +1,12 @@
+﻿using Application.Common.Constants;
+using MediatR;
+
+namespace Application.Usecases.Command
+{
+    public class UpdateTaskStatusCommand : IRequest<OperationResult<string?>>
+    {
+        public string TaskId { get; set; }
+        public string Status { get; set; }
+    }
+
+}
