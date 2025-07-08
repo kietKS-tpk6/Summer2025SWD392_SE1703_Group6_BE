4679157a8ec1aa39100c385984660ea5c8485033
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-07-08 23:09:01 +0700

    create CompleteTaskRequest

---
+++ Application/DTOs/UpdateTaskStatusRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class UpdateTaskStatusRequest
+    {
+        public string Status { get; set; } = string.Empty;
+        public string? LecturerID { get; set; }
+    }
+}
