768eff68e3049e8190e48d2158ef9be0d59cb8a6
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-07-08 19:38:51 +0700

    create GetTasksByLecturerIdQuery

---
+++ Application/Usecases/Command/GetTasksByLecturerIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using Application.Common.Constants;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Usecases.Query
+{
+    public class GetTasksByLecturerIdQuery : IRequest<OperationResult<List<WorkTask>>>
+    {
+        public string LecturerId { get; set; }
+    }
+}
