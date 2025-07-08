249802e6b6b6e68fc63aa612dc648a84bd4fbc56
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-07-08 19:25:03 +0700

    create ITaskService

---
+++ Application/IServices/ITaskService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Common.Constants;
+using Application.Usecases.Command;
+using Domain.Entities;
+
+namespace Application.IServices
+{
+    public interface ITaskService
+    {
+        Task<OperationResult<string?>> CreateTaskAsync(TaskCreateCommand command);
+        Task<OperationResult<List<WorkTask>>> GetTasksByLecturerIdAsync(string lecturerId);
+        Task<OperationResult<WorkTask?>> GetTaskByIdAsync(string taskId);
+        Task<OperationResult<string?>> UpdateTaskStatusAsync(string taskId, string status);
+        Task<OperationResult<List<WorkTask>>> GetAllTasksAsync();
+        Task<OperationResult<string?>> DeleteTaskAsync(string taskId);
+    }
+}
