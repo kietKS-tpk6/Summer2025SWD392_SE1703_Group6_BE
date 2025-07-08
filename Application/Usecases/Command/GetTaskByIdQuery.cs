0a7bcbd282cfa5eb25732b07adce97c3c20e711e
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-07-08 19:39:39 +0700

    create GetTaskByIdQuery

---
+++ Application/Usecases/Command/GetTaskByIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using Application.Common.Constants;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Usecases.Query
+{
+    public class GetTaskByIdQuery : IRequest<OperationResult<WorkTask?>>
+    {
+        public string TaskId { get; set; }
+    }
+}
