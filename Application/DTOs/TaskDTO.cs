cf2c229af343c6ce9464d8f1f090ad473c09cc76
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-07-08 19:48:22 +0700

    create TaskDTO

---
+++ Application/DTOs/TaskDTO.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Enums;
+
+namespace Application.DTOs
+{
+    public class TaskDTO
+    {
+        public string TaskID { get; set; }
+        public string Type { get; set; }
+        public string Content { get; set; }
+        public DateTime DateStart { get; set; }
+        public DateTime Deadline { get; set; }
+        public string? Note { get; set; }
+        public string? ResourcesURL { get; set; }
+        public string? AssignedLecturerID { get; set; }
+        public string? AssignedLecturerName { get; set; }
+        public string? Status { get; set; }
+        public bool IsOverdue => DateTime.Now > Deadline;
+        public int DaysUntilDeadline => (Deadline - DateTime.Now).Days;
+    }
+
+    public class TaskCreateDTO
+    {
+        public string LecturerID { get; set; }
+        public TaskType Type { get; set; }
+        public string Content { get; set; }
+        public DateTime DateStart { get; set; }
+        public DateTime Deadline { get; set; }
+        public string? Note { get; set; }
+        public string? ResourcesURL { get; set; }
+    }
+
+    public class TaskListDTO
+    {
+        public List<TaskDTO> Tasks { get; set; }
+        public int TotalTasks { get; set; }
+        public int CompletedTasks { get; set; }
+        public int PendingTasks { get; set; }
+        public int OverdueTasks { get; set; }
+    }
+}
