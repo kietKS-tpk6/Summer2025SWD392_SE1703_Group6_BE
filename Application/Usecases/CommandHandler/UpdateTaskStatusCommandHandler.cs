a64ff2fc43ac5240ef071d0db43287f0025d728d
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-07-08 19:46:35 +0700

    create UpdateTaskStatusCommandHandler

---
+++ Application/Usecases/CommandHandler/UpdateTaskStatusCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Common.Constants;
+using Application.IServices;
+using Application.Usecases.Command;
+using MediatR;
+
+namespace Application.Usecases.CommandHandler
+{
+    public class UpdateTaskStatusCommandHandler : IRequestHandler<UpdateTaskStatusCommand, OperationResult<string?>>
+    {
+        private readonly ITaskService _taskService;
+
+        public UpdateTaskStatusCommandHandler(ITaskService taskService)
+        {
+            _taskService = taskService;
+        }
+
+        public async Task<OperationResult<string?>> Handle(UpdateTaskStatusCommand request, CancellationToken cancellationToken)
+        {
+            return await _taskService.UpdateTaskStatusAsync(request.TaskId, request.Status);
+        }
+    }
+}
