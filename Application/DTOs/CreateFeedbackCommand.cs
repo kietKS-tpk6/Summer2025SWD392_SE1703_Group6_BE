ae2fae4203954829cc7e338755d25eb7a3d6376e
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-07-09 17:31:23 +0700

    create CreateFeedbackCommand

---
+++ Application/DTOs/CreateFeedbackCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Application.Usecases.Command
+{
+    public class CreateFeedbackCommand : IRequest<string>
+    {
+        public string ClassID { get; set; }
+        public string StudentID { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+    }
+}
