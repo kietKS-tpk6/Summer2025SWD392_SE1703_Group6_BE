9551922450b9c615902989b18f4ee6a626510bd1
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-07-09 17:39:33 +0700

    create IFeedbackService

---
+++ Application/IServices/IFeedbackService.cs
@@ -0,0 +1,20 @@
+﻿using Application.DTOs;
+using Application.Usecases.Command;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Application.IServices
+{
+    public interface IFeedbackService
+    {
+        Task<string> CreateFeedbackAsync(CreateFeedbackCommand command);
+        Task<List<FeedbackDTO>> GetFeedbacksByClassAsync(string classId);
+        Task<List<FeedbackDTO>> GetFeedbacksByStudentAsync(string studentId);
+        Task<FeedbackDTO> GetFeedbackByIdAsync(string feedbackId);
+        Task<FeedbackSummaryDTO> GetFeedbackSummaryByClassAsync(string classId);
+        Task<bool> HasStudentFeedbackForClassAsync(string studentId, string classId);
+        Task<string> GenerateNextFeedbackIdAsync();
+        Task<bool> DeleteFeedbackAsync(string feedbackId);
+        Task<bool> UpdateFeedbackAsync(string feedbackId, int rating, string comment);
+    }
+}
