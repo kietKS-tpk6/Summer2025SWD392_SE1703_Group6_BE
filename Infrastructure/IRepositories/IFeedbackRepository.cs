288b93aaffd9bfed11a3bde5ea8cd46c3cab3a3c
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-07-09 17:40:18 +0700

    create IFeedbackRepository

---
+++ Infrastructure/IRepositories/IFeedbackRepository.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Infrastructure.IRepositories
+{
+    public interface IFeedbackRepository
+    {
+        Task<string> CreateFeedbackAsync(Feedback feedback);
+        Task<Feedback> GetFeedbackByIdAsync(string feedbackId);
+        Task<List<Feedback>> GetFeedbacksByClassIdAsync(string classId);
+        Task<List<Feedback>> GetFeedbacksByStudentIdAsync(string studentId);
+        Task<bool> HasStudentFeedbackForClassAsync(string studentId, string classId);
+        Task<int> GetTotalFeedbacksCountAsync();
+        Task<double> GetAverageRatingByClassAsync(string classId);
+        Task<int> GetFeedbackCountByClassAsync(string classId);
+        Task<Dictionary<int, int>> GetRatingDistributionByClassAsync(string classId);
+        Task<bool> UpdateFeedbackAsync(Feedback feedback);
+        Task<bool> DeleteFeedbackAsync(string feedbackId);
+    }
+}
