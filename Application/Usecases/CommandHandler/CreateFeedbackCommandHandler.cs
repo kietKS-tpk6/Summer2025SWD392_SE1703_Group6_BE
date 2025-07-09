06cc23872909c12eb89731dfd21cdaa26085a928
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-07-09 17:40:53 +0700

    create CreateFeedbackCommandHandler

---
+++ Application/Usecases/CommandHandler/CreateFeedbackCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using Application.IServices;
+using Application.Usecases.Command;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Usecases.CommandHandler
+{
+    public class CreateFeedbackCommandHandler : IRequestHandler<CreateFeedbackCommand, string>
+    {
+        private readonly IFeedbackService _feedbackService;
+
+        public CreateFeedbackCommandHandler(IFeedbackService feedbackService)
+        {
+            _feedbackService = feedbackService;
+        }
+
+        public async Task<string> Handle(CreateFeedbackCommand request, CancellationToken cancellationToken)
+        {
+            return await _feedbackService.CreateFeedbackAsync(request);
+        }
+    }
+}
