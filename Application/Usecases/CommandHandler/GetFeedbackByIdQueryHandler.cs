259de26af10eea37b8b8bd944db9b59857562c0f
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-07-09 17:50:58 +0700

    create commandHandler for feedback

---
+++ Application/Usecases/CommandHandler/GetFeedbackByIdQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using Application.DTOs;
+using Application.IServices;
+using Application.Usecases.Command;
+using MediatR;
+
+namespace Application.Usecases.CommandHandler
+{
+    public class GetFeedbackByIdQueryHandler : IRequestHandler<GetFeedbackByIdQuery, FeedbackDTO>
+    {
+        private readonly IFeedbackService _feedbackService;
+
+        public GetFeedbackByIdQueryHandler(IFeedbackService feedbackService)
+        {
+            _feedbackService = feedbackService;
+        }
+
+        public async Task<FeedbackDTO> Handle(GetFeedbackByIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _feedbackService.GetFeedbackByIdAsync(request.FeedbackID);
+        }
+    }
+}
