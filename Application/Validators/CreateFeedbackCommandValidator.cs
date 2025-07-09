109d1b0ed2eefdfd456c6672eaec3e1b411e0024
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-07-09 17:38:42 +0700

    create CreateFeedbackCommandValidator

---
+++ Application/Validators/CreateFeedbackCommandValidator.cs
@@ -0,0 +1,33 @@
+﻿using Application.Usecases.Command;
+using FluentValidation;
+
+namespace Application.Validators
+{
+    public class CreateFeedbackCommandValidator : AbstractValidator<CreateFeedbackCommand>
+    {
+        public CreateFeedbackCommandValidator()
+        {
+            RuleFor(x => x.ClassID)
+                .NotEmpty()
+                .WithMessage("Class ID is required")
+                .MaximumLength(6)
+                .WithMessage("Class ID must not exceed 6 characters");
+
+            RuleFor(x => x.StudentID)
+                .NotEmpty()
+                .WithMessage("Student ID is required")
+                .MaximumLength(6)
+                .WithMessage("Student ID must not exceed 6 characters");
+
+            RuleFor(x => x.Rating)
+                .NotEmpty()
+                .WithMessage("Rating is required")
+                .InclusiveBetween(1, 5)
+                .WithMessage("Rating must be between 1 and 5");
+
+            RuleFor(x => x.Comment)
+                .MaximumLength(1000)
+                .WithMessage("Comment must not exceed 1000 characters");
+        }
+    }
+}
