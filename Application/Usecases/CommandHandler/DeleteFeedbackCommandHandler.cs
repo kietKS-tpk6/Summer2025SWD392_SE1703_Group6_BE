aa4701fa2f78a6ba1330333bb27929df5e16e623
hna-gnart <anhtntse183272@fpt.edu.vn>
2025-07-09 18:04:43 +0700

    create DeleteFeedbackCommandHandler

---
+++ Application/Usecases/CommandHandler/DeleteFeedbackCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.IServices;
+using Application.Usecases.Command;
+using MediatR;
+
+namespace Application.Usecases.CommandHandler
+{
+    public class DeleteFeedbackCommandHandler : IRequestHandler<DeleteFeedbackCommand, bool>
+    {
+        private readonly IFeedbackService _feedbackService;
+
+        public DeleteFeedbackCommandHandler(IFeedbackService feedbackService)
+        {
+            _feedbackService = feedbackService;
+        }
+
+        public async Task<bool> Handle(DeleteFeedbackCommand request, CancellationToken cancellationToken)
+        {
+            return await _feedbackService.DeleteFeedbackAsync(request.FeedbackID);
+        }
+    }
+}
